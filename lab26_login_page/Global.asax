5596630691dd6b7044f511c50814b8dc6fd044ef
saunlani <saunlani@gmail.com>
2018-06-05 16:12:44 -0400

    Add project files.

---
+++ lab26_login_page/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab26_login_page.MvcApplication" Language="C#" %>
---
+++ lab26_login_page/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab26_login_page.Startup))]
+namespace lab26_login_page
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
